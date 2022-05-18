ef4be90c7545c3a501111452a023aaab11ac1d4f
Lindsey Myers <candlmyers1012@gmail.com>
2022-05-18 09:51:44 -0600

    create all models

---
+++ models/video.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const videoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  videoType: { type: String },
+  description: { type: String },
+  link: { type: String },
+  releaseDate: { type: String }
+});
+
+module.exports = mongoose.model('video', videoSchema);
