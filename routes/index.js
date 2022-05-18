17d3545f131bc036a7b006b1ca831bbeac070ccf
McKay <wmsmckay@gmail.com>
2022-05-18 16:23:28 -0600

    added ignored routes for autogen

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,28 @@ const router = require('express').Router();
 // @desc    Login/Landing Page
 // @route   GET /
 router.get('/', (req, res) => {
+  // #swagger.ignore = true
   res.render('login', {
     layout: 'login'
   });
 });
-
 router.get('/dashboard', (req, res) => {
   res.render('dashboard', {
+    // #swagger.ignore = true
     name: req.user.firstName
   });
 });
+router.use(
+  // #swagger.ignore = true
+  '/auth',
+  require('./auth')
+);
+router.use(
+  // #swagger.ignore = true
+  '/api-docs',
+  require('./swagger')
+);
 
-router.use('/auth', require('./auth'));
-router.use('/api-docs', require('./swagger'));
 router.use('/audio', require('./audio'));
 router.use('/video', require('./video'));
 router.use('/document', require('./document'));
